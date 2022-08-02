4bf84a3e83f7de198e2ea77ad5e455f207a23919
slplahiru <toleara20@gmail.com>
2022-08-02 21:52:31 +0000

    random file serve fixed

--- sre/dummy-pdf-or-png/main.go
+++ sre/dummy-pdf-or-png/main.go
@@ -1,23 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 func serveRandomFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		log.Println("ERROR: Expected a GET request")
+		http.Error(w, "Not supported", 403)
+		return
+	}
+
+	err := error(nil)
+	if r.URL.String() != "/" {
+		_, err = strconv.Atoi(r.URL.String()[1:])
+	}
+	if err != nil {
+		log.Println("ERROR: Expected an integer path suffix")
+		http.Error(w, "Need an integer", 400)
+		return
+	}
+
 	rnd := rand.Intn(10)
 	if rnd < 5 {
 		http.ServeFile(w, r, "./dummy.png")
 		return
-	}
-	if rnd < 9 {
+	} else {
 		http.ServeFile(w, r, "./dummy.pdf")
 		return
 	}
-	http.ServeFile(w, r, "./corrupt-dummy.pdf")
 }
 
 func main() {
